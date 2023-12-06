7f0c9fe6a96e8de4d505ebeb0ec314cbd4a64f85
Frederic Bonnet <frederic.bonnet.fr@gmail.com>
2023-12-06 16:39:13 +0100

    init spring boot project

---
+++ src/main/java/com/myprojects/junittests/JunitTestsApplication.java
@@ -0,0 +1,13 @@
+package com.myprojects.junittests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunitTestsApplication.class, args);
+	}
+
+}
