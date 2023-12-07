b5538439513d8d8941ca5644b0c48b35eb441e49
Frederic Bonnet <frederic.bonnet.fr@gmail.com>
2023-12-07 10:17:50 +0100

    added java.util.function methods

--- src/main/java/com/myprojects/junittests/JunitTestsApplication.java
+++ src/main/java/com/myprojects/junittests/JunitTestsApplication.java
@@ -1,5 +1,10 @@
 package com.myprojects.junittests;
 
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
@@ -8,6 +13,7 @@ public class JunitTestsApplication {
 	public static void main(String[] args) {
 		Calculator myCalc = new Calculator();
 
+		// Calculator methods
 		Integer res1 = myCalc.sum(1, 1);
 		Integer res2 = myCalc.substract(10, 8);
 		Integer res3 = myCalc.multiply(6, 4);
@@ -17,5 +23,22 @@ public class JunitTestsApplication {
 		System.out.println("10 - 8 = " + res2);
 		System.out.println("6 * 4 = " + res3);
 		System.out.println("27 / 4 = " + res4);
+
+		// java.util.function methods
+		Function<String, Integer> strLengthFunction = str -> str.length();
+		int res5 = strLengthFunction.apply("Hello");
+		System.out.println("longueur de la chaîne 'Hello': " + res5);
+
+		Predicate<Integer> isEvenPredicate = num -> num % 2 == 0;
+		boolean res6 = isEvenPredicate.test(6);
+		System.out.println("6 is even : " + res6);
+
+		Consumer<String> stringPrinter = str -> System.out.println(str);
+		stringPrinter.accept("This is a java.util.function.Consumer test.");
+
+		Supplier<Integer> randomIntSupplier = () -> (int) (Math.random() * 100);
+		int randomNumber = randomIntSupplier.get();
+		System.out.println("java.util.function.Supplier test, with Math.random(): " + randomNumber);
+		
 	}
 }
